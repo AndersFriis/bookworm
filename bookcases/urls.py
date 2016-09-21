43b5f207f21e7af9c335a5797588048e41f2b2d0
Anders Friis <Friis73071@gmail.com>
2016-09-21 13:40:08 -0400

    merge conflict

--- bookcases/urls.py
+++ bookcases/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^$', views.bookcase_list, name="bookcase_list"),
     url(r'^(?P<id>\d+)/$', views.bookcase_detail, name="bookcase_detail"),
     url(r'^bookshelf/(?P<id>\d+)/$', views.bookshelf_detail, name="bookshelf_detail"),
->>>>>>> 79363db975d0e3a4d67075b6377f11b39c597667
+
 ]
